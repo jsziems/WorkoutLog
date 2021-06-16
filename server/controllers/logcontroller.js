59b9a49b5a6a4c308486cdd4ae1238ede2ea90ee
Janet Ziems <janetziems@Janets-MacBook-Air.local>
2021-06-16 12:42:21 -0400

    Added update and delete functionality.

--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -68,13 +68,96 @@ router.get("/:id", validateJWT, async (req, res) => {
     }
 })
 
+/*
+** Update a log
+*/
+router.put("/:id", validateJWT, async (req, res) => {
+    /* ToDo:  Handle errors properly.  Currently returns status 200 even if 
+    ** the id isn't found, or has a different owner_id.
+    */
+    const { description, definition, result } = req.body.log
+    const logId = req.params.id
+    const userId = req.user.id
+
+    const query = {
+        where: {
+            id: logId,
+            owner_id: userId
+        }
+    }
+
+    const updatedLog = {
+        description: description,
+        definition,
+        result
+    }
+
+    try {
+        let update = await LogModel.update(updatedLog, query)
+        if (update) {
+            res.status(200).json({
+                message: `Log for Id ${logId} has been updated`,
+                updatedLog
+            })
+        } else {
+            res.status(404).json({
+                message: 'Log Id not updated'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+/*
+** Delete a log
+*/
+router.delete("/:id", validateJWT, async (req, res) => {
+    /* ToDo:  Handle errors properly.  Currently returns status 200 even if 
+    ** the id isn't found, or has a different owner_id.
+    */
+    const logId = req.params.id
+    const userId = req.user.id
+
+    const query = {
+        where: {
+            id: logId,
+            owner_id: userId
+        }
+    }
+
+    try {
+        let logRemoved = await LogModel.destroy(query)
+        if (logRemoved) {
+            res.status(200).json({
+                message: `Log for Id ${logId} has been removed`
+            })
+        } else {
+            res.status(404).json({
+                message: 'Log Id not removed'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
 
 router.get('/practice', validateJWT, (req, res) => {
     res.send('Practice route!')
 })
 
+/*
+** About
+*/
 router.get('/about', (req, res) => {
     res.send('The About route!')
+})
+
+/*
+** Practice Route
+*/
+router.get('/practice', validateJWT, (req, res) => {
+    res.send('Practice route!')
 })
 
 module.exports = router
